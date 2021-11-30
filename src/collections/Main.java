544b59cbb22bcc60a1d2e1eadb22e088f5240258
KaterinaOsipenko <kate.osipenko30@gmail.com>
2021-11-30 13:50:06 +0200

    create class iterator

--- src/collections/Main.java
+++ src/collections/Main.java
@@ -4,5 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Implementation ArrayList, LinkedList, HashSet, TreeSet");
+        /* ArrayList - class interface List
+           LinkedList - class interface List and Deque
+           HashSet - class interface Set
+           TreeSet - class interface SortedSet
+         */
     }
 }
